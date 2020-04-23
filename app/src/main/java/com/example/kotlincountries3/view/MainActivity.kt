5f7f47282297cbbb4b2896f2a21d34d006d0e64a
Metin Yıldıran <metin.1464@gmail.com>
2020-04-23 19:47:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlincountries3/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kotlincountries3.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kotlincountries3.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
