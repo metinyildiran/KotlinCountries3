f57915f93ebf9b699e56370cb9156705a08ad24f
Metin Yıldıran <metin.1464@gmail.com>
2020-06-01 22:27:43 +0300

    23. ders bitti.

---
+++ app/src/main/java/com/example/kotlincountries3/model/Model.kt
@@ -0,0 +1,10 @@
+package com.example.kotlincountries3.model
+
+data class Country(
+    val countryName: String?,
+    val countryCapital: String?,
+    val countryRegion: String?,
+    val countryLanguage: String?,
+    val countryCurrency: String?,
+    val imageUrl: String?
+)
