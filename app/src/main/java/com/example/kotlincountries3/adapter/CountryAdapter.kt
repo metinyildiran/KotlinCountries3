933b5af0f0bcdd89cc62b4270704538a7fe94ae8
Metin Yıldıran <metin.1464@gmail.com>
2020-06-01 22:53:21 +0300

    24. ders bitti.

---
+++ app/src/main/java/com/example/kotlincountries3/adapter/CountryAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.kotlincountries3.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlincountries3.R
+import com.example.kotlincountries3.model.Country
+import kotlinx.android.synthetic.main.item_country.view.*
+
+class CountryAdapter(val countryList: ArrayList<Country>) : RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+
+    class CountryViewHolder(var view : View) : RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_country, parent, false)
+
+        return CountryViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return countryList.size
+    }
+
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+        holder.view.name.text = countryList[position].countryName
+        holder.view.region.text = countryList[position].countryRegion
+    }
+
+    fun updateCountryList(newCountryList : List<Country>){
+        countryList.clear()
+        countryList.addAll(newCountryList)
+        notifyDataSetChanged()
+    }
+}
