40909f1ed6e196dcff9995be3a60828e0b713d2e
Metin Yıldıran <metin.1464@gmail.com>
2020-06-02 12:35:08 +0300

    26. ders bitti.

--- app/src/main/java/com/example/kotlincountries3/view/FeedFragment.kt
+++ app/src/main/java/com/example/kotlincountries3/view/FeedFragment.kt
@@ -1,13 +1,23 @@
 package com.example.kotlincountries3.view
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlincountries3.R
+import com.example.kotlincountries3.adapter.CountryAdapter
+import com.example.kotlincountries3.viewmodel.FeedViewModel
+import kotlinx.android.synthetic.main.fragment_feed.*
 
 class FeedFragment : Fragment() {
+
+    private lateinit var viewModel: FeedViewModel
+    private val countryAdapter = CountryAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +34,21 @@ class FeedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProviders.of(this).get(FeedViewModel::class.java)
+        viewModel.refreshData()
+
+        countryList.layoutManager = LinearLayoutManager(context)
+        countryList.adapter = countryAdapter
+
+    }
+
+    fun observeLiveData() {
+        viewModel.countries.observe(this, Observer {
+            it?.let {
+                countryList.visibility = View.VISIBLE
+                countryAdapter.updateCountryList(it)
+
+            }
+        })
     }
 }
--- app/src/main/java/com/example/kotlincountries3/viewmodel/FeedViewModel.kt
+++ app/src/main/java/com/example/kotlincountries3/viewmodel/FeedViewModel.kt
@@ -9,4 +9,16 @@ class FeedViewModel : ViewModel() {
     val countryError = MutableLiveData<Boolean>()
     val countryLoading = MutableLiveData<Boolean>()
 
+    fun refreshData(){
+        val country = Country("Norway", "Europe", "Oslo", "NOK", "Norwegian", "www.website.no")
+        val country2 = Country("Sweden", "Europe", "Stockholm", "SEK", "Swedish", "www.website.se")
+        val country3 = Country("Turkey", "Asia", "Ankara", "TRY", "Turkish", "www.website.tr")
+
+        val countryList = arrayListOf<Country>(country, country2, country3)
+
+        countries.value = countryList
+        countryError.value = false
+        countryLoading.value = false
+    }
+
 }
